84b20e5cd27cc42c2e5440e8c975581103ff4fec
SAGAR SINGH <sagarsingh@SAGARs-MacBook-Air.local>
2025-11-29 19:08:01 +0530

    final changes in allowed host

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['django-greatkart-env.eba-mkcnpbmq.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-greatkart-env.eba-mkcnpbmq.us-west-2.elasticbeanstalk.com','*']
 
 
 # Application definition
