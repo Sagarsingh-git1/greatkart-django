b9dadc436485e624e53ce1900889c0efea84edd3
Sagar Singh <sagarcreane@gmail.com>
2025-12-04 02:04:43 +0530

    check

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -158,10 +158,9 @@ STATIC_ROOT = BASE_DIR / 'static'
 # Media FIles Configuration
 
 MEDIA_URL='/media/'
-if DEBUG:    
-    MEDIA_ROOT= BASE_DIR /'media'
-else:
-    MEDIA_ROOT = '/var/app/media'
+
+MEDIA_ROOT= BASE_DIR /'media'
+
  
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
