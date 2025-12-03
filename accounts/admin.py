138c1913a40029cba9de6e4b4eb81e7a69e9dd61
Sagar Singh <sagarcreane@gmail.com>
2025-12-04 00:27:33 +0530

    profile picture bug in the admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -18,7 +18,9 @@ class AccountAdmin(UserAdmin):
 
 class UserProfileAdmin(admin.ModelAdmin): 
     def thumbnail(self,object):
-        return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url))
+        if object.profile_picture and hasattr(object.profile_picture, 'url'):    
+            return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url))
+        return "No Image"
     thumbnail.short_description='Profile Picture'
 
     list_display=('thumbnail','user','city','state','country')
